2506620fbfd19b644e2188565949c3975f51cb4f
NoobBegintoIT <dti.tainhat09@gmail.com>
2025-05-11 03:40:36 +0700

    Add files via upload

--- src/BUS/InvoiceBUS.java
+++ src/BUS/InvoiceBUS.java
@@ -1,18 +1,14 @@
-
 package BUS;
 
-import DAO.*;
-import DTO.*;
-import GUI.*;
+import DAO.InvoiceDAO;
+import DTO.Invoice;
+import DTO.ListInvoice;
+import GUI.InvoicePanel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import org.jfree.chart.JFreeChart;
 
-/**
- *
- * @author ASUS
- */
 public class InvoiceBUS implements ActionListener {
     private ListInvoice listIv;
     private InvoicePanel jPanelInvoice;
@@ -35,11 +31,23 @@ public class InvoiceBUS implements ActionListener {
     private void search() { 
         String info = jPanelInvoice.getTextFieldSearch().getText().trim();
         ArrayList<Invoice> result = listIv.search(info);
+        if (result == null) {
+            result = new ArrayList<>();
+        }
         jPanelInvoice.showListInvoice(result);
         jPanelInvoice.showListInvoiceDetail(null);
+        jPanelInvoice.selectedRowIndex = -1;
     }
 
-    public JFreeChart createChart(){
+    public void loadInvoices() {
+        ArrayList<Invoice> result = InvoiceDAO.getAllInvoices();
+        listIv.setList(result);
+        jPanelInvoice.showListInvoice(result);
+        jPanelInvoice.showListInvoiceDetail(null);
+        jPanelInvoice.selectedRowIndex = -1;
+    }
+
+    public JFreeChart createChart() {
         return InvoiceDAO.createInvoiceChart();
     }
 }
